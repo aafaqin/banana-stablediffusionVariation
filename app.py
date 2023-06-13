723de9f97de14018059c31fdeaa680d487673471
aafaq <aafaq.in@gmail.com>
2023-06-13 22:55:45 +0530

    variation pipeline

--- app.py
+++ app.py
@@ -5,44 +5,64 @@ import base64
 from io import BytesIO
 import os
 
+from diffusers import StableDiffusionImageVariationPipeline
+from PIL import Image
+import torch
+from torchvision import transforms
+
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
+    global device
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
     
     # this will substitute the default PNDM scheduler for K-LMS  
-    lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
+    # lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
 
-    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN)
+    # model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN)
+
+    device = "cpu"
+    model = StableDiffusionImageVariationPipeline.from_pretrained(
+    "lambdalabs/sd-image-variations-diffusers",
+    revision="v2.0",
+    )
+    model = model.to(device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
+    global device
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    height = model_inputs.get('height', 512)
-    width = model_inputs.get('width', 512)
-    num_inference_steps = model_inputs.get('num_inference_steps', 50)
-    guidance_scale = model_inputs.get('guidance_scale', 7.5)
-    input_seed = model_inputs.get("seed",None)
-    
-    #If "seed" is not sent, we won't specify a seed in the call
-    generator = None
-    if input_seed != None:
-        generator = torch.Generator("cuda").manual_seed(input_seed)
-    
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    image_data = model_inputs.get('image_data', None)
+
+    if image_data == None:
+        return {'message': "No image bro!"}
     
     # Run the model
     with autocast("cuda"):
-        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator)["sample"][0]
-    
+       
+        im = Image.open(BytesIO(base64.b64decode(image_data))).convert("RGB")
+        tform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(
+            (224, 224),
+            interpolation=transforms.InterpolationMode.BICUBIC,
+            antialias=False,
+            ),
+        transforms.Normalize(
+        [0.48145466, 0.4578275, 0.40821073],
+        [0.26862954, 0.26130258, 0.27577711]),
+        ])
+        inp = tform(im).to(device).unsqueeze(0)
+
+    out = model(inp, guidance_scale=3)
+    out["images"][0].save("result_s.jpg")
+
     buffered = BytesIO()
-    image.save(buffered,format="JPEG")
+    out["images"][0].save(buffered,format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     # Return the results as a dictionary
--- test.py
+++ test.py
@@ -5,8 +5,16 @@ import requests
 import base64
 from io import BytesIO
 from PIL import Image
+import sys
 
-model_inputs = {'prompt': 'realistic field of grass'}
+# img_name = sys.argv[1:][0]   # to load from argv
+
+img_name="../2024_gmc_hummer_ev_suv_4k-t2.jpg"
+with open(img_name, "rb") as f:
+    bytes = f.read()
+    encoded = base64.b64encode(bytes).decode('utf-8')
+
+model_inputs = {'image_data': encoded}
 
 res = requests.post('http://localhost:8000/', json = model_inputs)
 
@@ -15,4 +23,4 @@ image_byte_string = res.json()["image_base64"]
 image_encoded = image_byte_string.encode('utf-8')
 image_bytes = BytesIO(base64.b64decode(image_encoded))
 image = Image.open(image_bytes)
-image.save("output.jpg")
+image.save("output_client.jpg")
